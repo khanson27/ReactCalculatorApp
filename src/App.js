5df3fbecb77c97a2d7559bf6639ed358603caaa4
khanson27 <hansonkm94@gmail.com>
2022-10-25 20:15:56 +0100

    added both components to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Keyboard } from "./components/Keyboard";
+import { useState } from "react";
+import { Display } from "./components/Display";
+import { Container } from "@mui/material";
 
 function App() {
+  const [inputOneNum, setInputOneNum] = useState("");
+  const [inputTwoNum, setInputTwoNum] = useState("");
+  const [operator, setOperator] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container
+        className="app"
+        style={{
+          width: "fit-content",
+          height: "fit-content",
+        }}
+      >
+        <Display
+          inputOneNum={inputOneNum}
+          operator={operator}
+          inputTwoNum={inputTwoNum}
+        />
+        <Keyboard
+          setInputOneNum={setInputOneNum}
+          setInputTwoNum={setInputTwoNum}
+          setOperator={setOperator}
+          inputOneNum={inputOneNum}
+          inputTwoNum={inputTwoNum}
+          operator={operator}
+        />
+      </Container>
+    </>
   );
 }
 
